c3b5055a1a88b6dad79db025be7b00e4fbbf7486
Bernd Matzner <mail@berndmatzner.de>
2019-04-28 12:14:20 +0200

    Version bump, upgrade packages, enhance CS

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var through = require('through2');
 module.exports = function() {
   return through.obj(function(file, enc, cb) {
     var fileDate = new Date();
-    fs.utimes(file.path, fileDate, fileDate, function(){});
+
+    fs.utimes(file.path, fileDate, fileDate, function() {});
+
     return cb(null, file);
   });
 };
